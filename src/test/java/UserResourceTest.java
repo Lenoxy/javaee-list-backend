73b7d86955c14b7b80811442e2905db34c5b3d00
lenoxy <lenoxy@gmx.ch>
2021-04-16 07:54:39 +0200

    Start work on loginTest, Mock not working yet

--- src/test/java/UserResourceTest.java
+++ src/test/java/UserResourceTest.java
@@ -1,16 +1,35 @@
+import dto.UserDto;
+import dto.a;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import repository.UserRepository;
 
-import service.DatabaseService;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class UserResourceTest{
 
-    private final DatabaseService databaseServiceMock = mock(DatabaseService.class);
+    @Mock
+    private final UserRepository userRepositoryMock = mock(UserRepository.class);
 
-    //@Test
-    public void loginTest(){
-        // TODO first: Make code more testable
-        //when(databaseServiceMock.entityManager.)
+    // Mock not working yet for obvious reasons
+
+    @Test
+    void loginTest(){
+        UserDto userDto = new UserDto(0, "c3d4856944538698d6cf217896afb58df4478e0f902a1c84bf897779c644deec", "username", null);
+        // TODO This mock is nice and well, but never injected into the sut instance, so the test throws a NullPointer
+        when(userRepositoryMock.get(userDto)).thenReturn(userDto);
+        UserResource sut = new UserResource();
+
+        String jwt = (String) sut.login(userDto).getEntity();
+
+        assertThat(jwt.length()).isEqualTo(64);
+
+    }
+
+    @Test
+    void isCorrectCredentialsTest(){
+
 
     }
 }
